b2682b2d42568cc7a575cb5de5acae09a37e9905
rajarshi-mahato <rajarshi.mahato12@gmail.com>
2025-07-12 16:31:16 +0530

    Grade.c
    Calculate the grade of the student depending on the marks entered.

--- KG1.c
+++ KG1.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 void main()
 {
-int n;
-printf("Enter your Marks");
-scanf("%d",&n);
-if(n>=90 && n<=100)
-printf("Your Grade is O");
-else if(n>=80 && n<=89)
-printf("Your Grade is E");
-else if(n>=70 && n<=79)
-printf("Your Grade is A");
-else if(n>=60 && n<=69)
-printf("Your Grade is B");
-else if(n>=50 && n<=59)
-printf("Your Grade is C");
-else if(n>=40 && n<=49)
-printf("Your Grade is D");
-else if(n>=0 && n<=39)
-printf("Your Grade is F");
-else
-printf("Invalid Input");
+      int n;
+      printf("Enter your Marks");
+      scanf("%d",&n);
+      if(n>=90 && n<=100)
+        printf("Your Grade is O");
+      else if(n>=80 && n<=89)
+        printf("Your Grade is E");
+      else if(n>=70 && n<=79)
+        printf("Your Grade is A");
+      else if(n>=60 && n<=69)
+        printf("Your Grade is B");
+      else if(n>=50 && n<=59)
+        printf("Your Grade is C");
+      else if(n>=40 && n<=49)
+        printf("Your Grade is D");
+      else if(n>=0 && n<=39)
+        printf("Your Grade is F");
+      else
+        printf("Invalid Input");
 }
